7c943fc90bfc8607f373e3bb9519e63a71cb1c71
hhri-yysh <heorhiiiukrainian2006@proton.me>
2025-03-16 13:49:59 +0200

    update lab3. lab3 is done already.

--- lab3/yashchuk_lab3.s
+++ lab3/yashchuk_lab3.s
@@ -7,7 +7,70 @@
 	E:	.byte 0xAA
 	F:	.byte 0x89
 	K	= 0xA77627	
+message:
+	.ascii " Yashchuk Heorhi\n"
+message_len = . - message
 
-.section .code
+
+.section .data
+	X:	.long 0
+	buffer:	.space 12
+
+.section .text
 _start:
-	; X = A2 * B1 + C4 / ( K - E1 * F1 ) 
+	movswl A, %edx
+	movsbl B, %eax
+	mov %edx, %ecx
+	imul %eax, %ecx
+
+	movsbl E, %edx
+	movsbl F, %eax
+	imul %edx, %eax
+
+	mov $K, %edx
+	mov %edx, %esi
+	sub %eax, %esi
+
+	mov C, %eax
+	cdq
+	idiv %esi
+	add %ecx, %eax
+	mov %eax, X
+
+	mov X, %eax
+	mov $buffer, %rdi
+	call _to_ascii
+
+	mov $1, %rax
+	mov $1, %rdi
+	mov $buffer, %rsi
+	mov $12, %rdx
+	syscall
+	
+	mov $1, %rax
+	mov $1, %rdi
+	mov $message, %rsi
+	mov $message_len, %rdx
+	syscall
+
+	mov $60, %rax
+	xor %rdi, %rdi
+	syscall
+
+_to_ascii:
+	mov $buffer+11, %rdi
+	movb $0, (%rdi)
+	dec %rdi
+	mov $10, %ecx
+
+.loop:
+	xor %edx, %edx
+        div %ecx
+        add $'0', %dl
+        mov %dl, (%rdi)
+        dec %rdi
+        test %eax, %eax
+        jnz .loop
+
+        inc %rdi
+        ret
